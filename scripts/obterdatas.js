97b3013174979268c2f61b9c3629f68616b9c9b2
jlvieira-byu <jvieira2@byupathway.edu>
2026-07-16 00:02:57 -0300

    preparação tarefa s01 Página Inicial

---
+++ scripts/obterdatas.js
@@ -0,0 +1,6 @@
+//const anocorrente = new Date().getFullYear();
+//document.getElementById('ano-atual').textContent = anocorrente;
+
+document.getElementById('ano-atual').textContent = new Date().getFullYear();
+
+document.getElementById('ultima-modificacao').textContent = document.lastModified;
