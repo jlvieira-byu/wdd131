6d346f399a7772b5fb4754a69ace574b73bc4272
jlvieira-byu <jvieira2@byupathway.edu>
2026-07-19 17:04:34 -0300

    s02 aprendizagem 3

--- semana02/scripts/bom.js
+++ semana02/scripts/bom.js
@@ -5,12 +5,26 @@ const meu_ul = document.querySelector('#list');
 const novo_li = document.createElement('li');
 const novo_botao_excluir= document.createElement('button');
 
-novo_li.textContent = meu_input.value;
 
-novo_botao_excluir.textContent = '❌';
-novo_botao_excluir.setAttribute('aria-label', 'Excluir item'); //tentar fazer alterar esse aria-label com o valor respectivo do capútlo de escritura adicionado
 
-novo_li.append(novo_botao_excluir);
+meu_button.addEventListener('click', function () {
+    if meu_input.value.trim() !== "" {
+        novo_li.textContent = meu_input.value;
 
-meu_ul.append(novo_li);
+        novo_botao_excluir.textContent = '❌';
+        novo_botao_excluir.setAttribute('aria-label', `Excluir item ${novo_li.textConten}`);
 
+        novo_li.append(novo_botao_excluir);
+
+        meu_ul.append(novo_li);
+
+        meu_input.value = '';
+    } else {
+        meu_input.focus();
+    }
+});
+
+novo_botao_excluir.addEventListener('click', function () {
+    meu_ul.removeChild(novo_li);
+    meu_input.focus();
+});
