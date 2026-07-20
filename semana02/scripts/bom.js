bc74ac36068f706a0271b5fe1753d9f8f4ba8e34
jlvieira-byu <jvieira2@byupathway.edu>
2026-07-20 08:56:38 -0300

    s02 aprendizagem 4

--- semana02/scripts/bom.js
+++ semana02/scripts/bom.js
@@ -2,29 +2,32 @@ const meu_input = document.querySelector('#favchap');
 const meu_button = document.querySelector('button');
 const meu_ul = document.querySelector('#list');
 
-const novo_li = document.createElement('li');
-const novo_botao_excluir= document.createElement('button');
-
-
-
 meu_button.addEventListener('click', function () {
-    if meu_input.value.trim() !== "" {
+    if (meu_input.value.trim() !== "") {
+        const novo_li = document.createElement('li');
+        const novo_botao_excluir = document.createElement('button');
+
         novo_li.textContent = meu_input.value;
-
         novo_botao_excluir.textContent = '❌';
-        novo_botao_excluir.setAttribute('aria-label', `Excluir item ${novo_li.textConten}`);
+        
+        novo_botao_excluir.setAttribute('aria-label', `Excluir item ${novo_li.textContent}`);
 
+        // adiciona botão dentro do novo li
         novo_li.append(novo_botao_excluir);
 
         meu_ul.append(novo_li);
 
+        novo_botao_excluir.addEventListener('click', function () {
+            meu_ul.removeChild(novo_li);
+            meu_input.focus();
+        });
+
         meu_input.value = '';
+        meu_input.focus();
+
     } else {
         meu_input.focus();
     }
 });
 
-novo_botao_excluir.addEventListener('click', function () {
-    meu_ul.removeChild(novo_li);
-    meu_input.focus();
-});
+
